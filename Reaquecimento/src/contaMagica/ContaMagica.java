f781aa1627be96bb84ad0c367a7c96c9416ff287
13108270 <13108270@W1879030.portoalegre.pucrsnet.br>
2017-03-21 22:23:19 -0300

    Conta Magica + Parquimetro

---
+++ Reaquecimento/src/contaMagica/ContaMagica.java
@@ -0,0 +1,18 @@
+package contaMagica;
+
+enum Categoria{Silver, Gold, Platinum}
+
+public class ContaMagica {
+	
+	private String nomeCliente;
+	private double saldo;
+	private Categoria status;
+	private double valor;
+	
+//	 public ContaMagica(String nome){}
+//	 public String getNomeCliente(){…}
+//	 public BigDecimal getSaldo(){…}
+//	 public Categorias getStatus(){…}
+//	 public void deposito(BigDecimal valor) {…}
+//	 public void retirada(BigDecimal valor) {…}
+	}
